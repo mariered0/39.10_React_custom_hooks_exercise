0071241234f2bc969238d38559746425b764982f
mariered0 <marieakg@gmail.com>
2023-01-03 19:27:33 -0800

    add useFlip and refactor PlayingCard/PokemonCard components

---
+++ src/hooks/useFlip.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+
+const useFlip = () => {
+    const [state, setState] = useState(true);
+    const toggleState = () => {
+        setState(state => !state);
+  };
+  return [state, toggleState]
+}
+
+export default useFlip;
